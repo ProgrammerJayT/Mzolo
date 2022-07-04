7e5e3036c384ce7aa5fa97bffa5625b47b9ce701
ProgrammerJayT <theanthem8@gmail.com>
2022-07-05 00:50:42 +0200

    Imported Login and Registration

--- Mzolo_client/src/routes/RouteStack.js
+++ Mzolo_client/src/routes/RouteStack.js
@@ -2,6 +2,8 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Splash from '../screens/Splash/Splash';
+import Registration from '../screens/Registration/Registration';
+import Login from '../screens/Login/Login';
 
 const Stack = createNativeStackNavigator();
 
@@ -13,6 +15,8 @@ function RouteStack() {
           headerShown: false,
         }}>
         <Stack.Screen name="Splash" component={Splash} />
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Registration" component={Registration} />
       </Stack.Navigator>
     </NavigationContainer>
   );
